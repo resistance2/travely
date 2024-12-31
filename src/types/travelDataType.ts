32157954dc8df13e0a0551a5a2322c0dd39580eb
ssumanlife <ksm9805@naver.com>
2024-12-31 15:16:02 +0900

    feat: api response타입 추가 (#140)

--- src/types/travelDataType.ts
+++ src/types/travelDataType.ts
@@ -1,3 +1,5 @@
+import { Review } from './reviewType';
+
 type Status = 'waiting' | 'approved' | 'rejected';
 
 export interface PageData {
@@ -32,4 +34,35 @@ export interface ApplicationUserData {
   phoneNumber: string;
   userId: string;
   appliedAt: string;
+}
+
+export interface GuideData {
+  userId: string;
+  userProfileImage: string;
+  socialName: string;
+  userEmail: string;
+  guideTotalRating: number;
+}
+
+export interface TravelDetailData {
+  guide: GuideData;
+  title: string;
+  content: string;
+  price: number;
+  thumbnail: string;
+  tag: string[];
+  travelCourse: string[];
+  includedItems: string[] | null;
+  excludedItems: string[] | null;
+  meetingTime: string[] | null;
+  meetingPlace: string | null;
+  FAQ: { question: string; answer: string }[] | null;
+  reviews: Review[];
+  team: Pick<
+    TravelTeamData,
+    'teamId' | 'travelStartDate' | 'travelEndDate' | 'personLimit' | 'approvedUsers'
+  >[];
+  totalRating: number;
+  bookmark: number;
+  isBookmark: boolean;
 }
