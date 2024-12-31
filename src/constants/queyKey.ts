5fc45f89999729ffbb1450d38160d11eb446ee69
ssumanlife <ksm9805@naver.com>
2024-12-31 17:05:30 +0900

    fix: 쿼리키 상수화 (#145)

--- src/constants/queyKey.ts
+++ src/constants/queyKey.ts
@@ -2,3 +2,4 @@ export const IMAGE_UPLOAD = 'image-upload';
 export const MANAGE_TRAVEL = 'manage-travel';
 export const MANAGE_TRAVEL_TEAMS = 'manage-travel-teams';
 export const TRAVEL_DETAIL = 'travel-detail';
+export const MY_CREATED_TRAVEL = 'my-created-travel';
