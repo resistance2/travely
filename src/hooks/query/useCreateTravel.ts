8d74bdd94cc486370d6283acbc75417c5db2622e
Haeun <haruyam15@gmail.com>
2025-01-08 17:40:48 +0900

    feat: 함께 떠나요 글 작성 후 데이터리셋 로직추가, 상세페이지 이동 링크수정 (#149)

--- src/hooks/query/useCreateTravel.ts
+++ src/hooks/query/useCreateTravel.ts
@@ -1,19 +1,24 @@
 import createTravel from '@/api/addTravel/createTravel';
+import useResetAddTravel from '@/hooks/custom/useResetAddTravel';
+
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 const useCreateTravel = () => {
   const navigate = useNavigate();
+  const resetAddTravel = useResetAddTravel().resetAddTravel;
+
   return useMutation({
     mutationFn: createTravel,
     onSuccess: (data) => {
       if (data) {
         alert('글 작성 완료');
-        navigate('/');
+        resetAddTravel();
+        navigate('/travel-list');
       }
     },
     onError: () => {
-      alert('글 작성 실패!!!!!!!!');
+      alert('글 작성에 실패했습니다.');
     },
   });
 };
--- src/stores/useAddTravelStore.ts
+++ src/stores/useAddTravelStore.ts
@@ -1,28 +1,34 @@
+import useUserStore from '@/stores/useUserStore';
 import { AddTravelData } from '@/types/travelDataType';
 import { create } from 'zustand';
 
 interface IUseAddTravelStore {
   data: AddTravelData;
   setData: (updates: Partial<AddTravelData>) => void;
+  resetData: () => void;
 }
 
+const userId = useUserStore.getState().user?.userId || null;
+const initData = {
+  userId,
+  thumbnail: null,
+  travelTitle: null,
+  travelContent: null,
+  travelCourse: null,
+  tag: null,
+  team: null,
+  travelPrice: 0,
+  includedItems: null,
+  excludedItems: null,
+  meetingTime: null,
+  FAQ: null,
+  meetingPlace: null,
+};
+
 const useAddTravelStore = create<IUseAddTravelStore>((set) => ({
-  data: {
-    userId: null,
-    thumbnail: null,
-    travelTitle: null,
-    travelContent: null,
-    travelCourse: null,
-    tag: null,
-    team: null,
-    travelPrice: 0,
-    includedItems: null,
-    excludedItems: null,
-    meetingTime: null,
-    FAQ: null,
-    meetingPlace: null,
-  },
+  data: initData,
   setData: (updates) => set((state) => ({ data: { ...state.data, ...updates } })),
+  resetData: () => set({ data: initData }),
 }));
 
 export default useAddTravelStore;
