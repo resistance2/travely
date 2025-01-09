a742309f06df62ddc8a46beaffa172b592c98a69
ssumanlife <ksm9805@naver.com>
2025-01-10 00:15:49 +0900

    feat: 데이터 타입 정의 (#171)

--- src/types/guideFindDataType.ts
+++ src/types/guideFindDataType.ts
@@ -1,6 +1,9 @@
 import { TravelTeamData } from './travelDataType';
 
-export type TeamData = Pick<TravelTeamData, 'travelStartDate' | 'travelEndDate' | 'personLimit'>;
+export type TeamData = Pick<
+  TravelTeamData,
+  'teamId' | 'travelStartDate' | 'travelEndDate' | 'personLimit'
+>;
 
 export interface AddForFindGuideData {
   userId: string | null;
@@ -8,4 +11,30 @@ export interface AddForFindGuideData {
   travelContent: string;
   thumbnail: string | null;
   team: TeamData[] | null;
+}
+
+export interface Author {
+  userId: string;
+  userProfileImage: string;
+  socialName: string;
+  userScore: number | null;
+}
+
+export interface Comment {
+  commentId: string;
+  userId: string;
+  socialName: string;
+  userProfileImage: string | null;
+  updatedAt: string;
+  comment: string;
+}
+
+export interface FindGuideDetailData {
+  author: Author;
+  title: string;
+  content: string;
+  thumbnail: string | null;
+  team: TeamData[] | null;
+  createdAt: string;
+  commentList: Comment[] | null;
 }
