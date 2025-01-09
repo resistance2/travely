f9688032a8b3dc9e616b21bb77fd5750198e27b7
ssumanlife <ksm9805@naver.com>
2025-01-09 12:34:02 +0900

    fix: 이미지 업로드 훅 및 api 수정 (#170)

--- src/utils/prepareImageUpload.ts
+++ src/utils/prepareImageUpload.ts
@@ -1,37 +1,24 @@
 import { ImageStore } from '@/stores/useImageStore';
 
-const addImageToFormData = (formData: FormData, type: string, base64String: string) => {
-  base64String = base64String || '';
-  if (!base64String) return;
+const formData = new FormData();
 
-  const [mimeString, base64Data] = base64String.split(',');
-  const byteString = atob(base64Data);
-  const ab = new Uint8Array(byteString.length);
-
-  for (let i = 0; i < byteString.length; i++) {
-    ab[i] = byteString.charCodeAt(i);
-  }
-
-  const file = new Blob([ab], { type: mimeString.split(':')[1].split(';')[0] });
-  const mimeType = mimeString.split(':')[1].split(';')[0];
-  const extension = mimeType.split('/')[1];
-  const timestamp = new Date().getTime();
-  formData.append(type, file, `${type}-${timestamp}.${extension}`);
+const addImageToFormData = (type: string, file: File) => {
+  if (!file) return;
+  formData.append(type, file);
 };
 
 const prepareImageUpload = (images: ImageStore) => {
-  const formData = new FormData();
   if (images.thumbnail) {
-    addImageToFormData(formData, 'thumbnail', images.thumbnail);
+    addImageToFormData('thumbnail', images.thumbnail);
   }
   if (images.meetingSpace) {
-    addImageToFormData(formData, 'meetingSpace', images.meetingSpace);
+    addImageToFormData('meetingSpace', images.meetingSpace);
   }
-  images.introSrcs.forEach((src, index) => {
-    if (src) {
-      addImageToFormData(formData, `introSrcs[${index}]`, src);
-    }
-  });
+  if (images.introSrcs.length > 0) {
+    images.introSrcs.forEach((src) => {
+      addImageToFormData('introSrcs', src);
+    });
+  }
 
   return formData;
 };
