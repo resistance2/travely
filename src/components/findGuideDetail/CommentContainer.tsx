c1a85ea8d11c9fbcdf3b3b20f3396da414b5ab95
ssumanlife <ksm9805@naver.com>
2025-01-13 11:25:40 +0900

    feat: 페이지네이션 기능 추가 및 api 적용 (#184)

--- src/components/findGuideDetail/CommentContainer.tsx
+++ src/components/findGuideDetail/CommentContainer.tsx
@@ -1,19 +1,44 @@
+import Pagination from '@mui/material/Pagination';
 import CommentList from './CommentList';
 import WritingComment from './WritingComment';
-import { Comment } from '@/types/guideFindDataType';
 import { css } from '@emotion/react';
+import useGetCommentList from '@/hooks/query/useGetCommentList';
+import { useState } from 'react';
 
 interface CommentContainerProps {
-  commentListData: Comment[];
   guidePostId: string;
 }
 
-const CommentContainer = ({ commentListData, guidePostId }: CommentContainerProps) => {
+const CommentContainer = ({ guidePostId }: CommentContainerProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data: commentData, refetch } = useGetCommentList(guidePostId as string, currentPage);
+
+  const handlePage = async (_: React.ChangeEvent<unknown>, page: number) => {
+    await setCurrentPage(page);
+    refetch();
+  };
+
   return (
     <div css={container}>
       <h2>댓글</h2>
       <WritingComment guidePostId={guidePostId} />
-      <CommentList commentListData={commentListData} guidePostId={guidePostId} />
+      <div css={listWrapper}>
+        {commentData?.pageInfo && commentData?.commentList ? (
+          <>
+            <CommentList commentListData={commentData.commentList} guidePostId={guidePostId} />
+            <Pagination
+              count={commentData.pageInfo.totalPages || 0}
+              page={currentPage}
+              onChange={handlePage}
+              color="primary"
+              showFirstButton
+              showLastButton
+            />
+          </>
+        ) : (
+          <p css={noList}>등록된 댓글이 없습니다.</p>
+        )}
+      </div>
     </div>
   );
 };
@@ -29,4 +54,18 @@ const container = css`
     font-size: 18px;
     font-weight: 600;
   }
+`;
+
+const listWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const noList = css`
+  margin: 26px auto;
+  color: #888;
+  font-size: 16px;
+  font-weight: 600;
 `;
