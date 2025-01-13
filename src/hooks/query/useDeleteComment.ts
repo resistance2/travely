c2d116c459480500a37cabbea5fc42c9c69c328f
ssumanlife <ksm9805@naver.com>
2025-01-13 11:28:38 +0900

    fix: 쿼리키 수정 (#184)

--- src/hooks/query/useDeleteComment.ts
+++ src/hooks/query/useDeleteComment.ts
@@ -1,6 +1,6 @@
 import deleteComment from '@/api/findGuideDetail/deleteComment';
 import { ShowToast } from '@/components/Toast';
-import { FIND_GUIDE_DETAIL } from '@/constants/queyKey';
+import { COMMENT_LIST } from '@/constants/queyKey';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const useDeleteComment = (guidePostId: string) => {
@@ -9,7 +9,7 @@ const useDeleteComment = (guidePostId: string) => {
     mutationFn: ({ commentId }: { commentId: string }) => deleteComment(commentId),
     onSuccess: () => {
       ShowToast('댓글이 삭제되었습니다.', 'success');
-      queryClient.invalidateQueries({ queryKey: [FIND_GUIDE_DETAIL, guidePostId] });
+      queryClient.invalidateQueries({ queryKey: [COMMENT_LIST, guidePostId] });
     },
     onError: () => {
       ShowToast('댓글 삭제가 실패했습니다. 잠시 후 다시 시도해주세요.', 'failed');
--- src/hooks/query/usePatchComment.ts
+++ src/hooks/query/usePatchComment.ts
@@ -1,6 +1,7 @@
 import patchComment from '@/api/findGuideDetail/patchComment';
 import { ShowToast } from '@/components/Toast';
-import { FIND_GUIDE_DETAIL } from '@/constants/queyKey';
+import { COMMENT_LIST } from '@/constants/queyKey';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 interface UsePatchCommentProps {
@@ -16,7 +17,7 @@ const usePatchComment = (guidePostId: string) => {
       patchComment(commentId, userId, comment),
     onSuccess: () => {
       ShowToast('댓글이 수정되었습니다.', 'success');
-      queryClient.invalidateQueries({ queryKey: [FIND_GUIDE_DETAIL, guidePostId] });
+      queryClient.invalidateQueries({ queryKey: [COMMENT_LIST, guidePostId] });
     },
     onError: () => {
       ShowToast('댓글 수정에 실패했습니다. 잠시 후 다시 시도해주세요.', 'failed');
